4944c6f9fbb5d4b40cdbb3c8b04cfec641859321
Pablo Maribondo <pablomgvieira@gmail.com>
2021-07-03 21:40:05 -0300

    feat: add types file

---
+++ src/types.d.ts
@@ -0,0 +1,15 @@
+interface DriversData {
+  number: number;
+  name: string;
+  team: string;
+  country: string;
+  dob: string;
+  placeOfBirth: string;
+}
+
+interface TracksData {
+  name: string;
+  country: string;
+  length: number;
+  numberOfLaps: number;
+}
